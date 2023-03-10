4b062a3eccfb72cf3f1bea975e4b1411f8530618
Andrew Zheng <azswim@live.unc.edu>
2023-03-10 14:39:25 -0500

    tutorial

--- notes.py
+++ notes.py
@@ -1,2 +1,99 @@
 # Use this file for notes and running examples...
 # As expected, run it with `python3 -m notes`
+
+# Engine manages connections to database
+from sqlalchemy import create_engine
+engine = create_engine("sqlite+pysqlite:///:memory:", echo=True)
+print(engine)
+
+from sqlalchemy import text
+# Create a connection to database
+with engine.connect() as conn:
+    result = conn.execute(text("select 'hello world'"))
+    print(result.all())
+
+with engine.connect() as conn:
+    conn.execute(text("CREATE TABLE some_table (x int, y int)"))
+    conn.execute(
+        text("INSERT INTO some_table (x, y) VALUES (:x, :y)"),
+        [{"x": 1, "y": 1}, {"x": 2, "y": 4}]
+    )
+    conn.commit()
+
+with engine.begin() as conn:
+    conn.execute(
+        text("INSERT INTO some_table (x, y) VALUES (:x, :y)"),
+        [{"x": 6, "y": 8}, {"x": 9, "y": 10}],
+    )
+
+with engine.connect() as conn:
+    result = conn.execute(text("SELECT x, y FROM some_table"))
+    for row in result:
+        print(f"x: {row.x}  y: {row.y}")
+
+with engine.connect() as conn:
+    result = conn.execute(text("SELECT x, y FROM some_table WHERE y > :y"), {"y": 4})
+    for row in result:
+        print(f"x: {row.x}  y: {row.y}")
+
+with engine.connect() as conn:
+    conn.execute(
+        text("INSERT INTO some_table (x, y) VALUES (:x, :y)"),
+        [{"x": 11, "y": 12}, {"x": 13, "y": 14}],
+    )
+    conn.commit()
+
+from sqlalchemy.orm import Session
+
+stmt = text("SELECT x, y FROM some_table WHERE y > :y ORDER BY x, y")
+with Session(engine) as session:
+    result = session.execute(stmt, {"y": 6})
+    for row in result:
+        print(f"x: {row.x}  y: {row.y}")
+
+with Session(engine) as session:
+    result = session.execute(
+        text("UPDATE some_table SET y=:y WHERE x=:x"),
+        [{"x": 9, "y": 11}, {"x": 13, "y": 15}],
+    )
+    session.commit()
+
+# with Session(engine) as session:
+#     result = session.execute(
+#         text("SELECT x, y FROM some_table WHERE y > :y ORDER BY x, y"), {"y": 6})
+#     for row in result:
+#         print(f"x: {row.x}  y: {row.y}")
+
+# Metadata keeps tracks of all the tables
+from sqlalchemy import MetaData
+metadata_obj = MetaData()
+
+from sqlalchemy import Table, Column, Integer, String
+user_table = Table(
+    "user_account",
+    metadata_obj,
+    Column("id", Integer, primary_key=True),
+    Column("name", String(30)),
+    Column("fullname", String),
+)
+
+from sqlalchemy import ForeignKey
+address_table = Table(
+    "address",
+    metadata_obj,
+    Column("id", Integer, primary_key=True),
+    Column("user_id", ForeignKey("user_account.id"), nullable=False),
+    Column("email_address", String, nullable=False),
+)
+
+print(repr(user_table.c.id))
+print(user_table.primary_key)
+
+# print(user_table.c.keys())
+
+metadata_obj.create_all(engine)
+
+# When you make a class that extends Base, they will be established as a new ORM mapped class
+from sqlalchemy.orm import DeclarativeBase
+class Base(DeclarativeBase):
+    pass
